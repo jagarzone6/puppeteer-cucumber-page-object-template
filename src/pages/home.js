a0b5a012165610f3de9bf8668d1d3a2c471965fe
jagarzone6 <jagarzone@gmail.com>
2019-08-11 22:51:36 -0500

    added clickOnCreateEmployee funtion to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,13 @@ class HomePage {
         this.LOGO = "div#logo_text img[alt='GAP Vacations Management']";
         this.USER_LOGGED_SPAN = 'div#menubar ul#user_information span';
         this.USER_LOGGED_SIGN_OUT_LINK = "div#menubar ul#user_information span a[data-method='delete']";
-        this.CREATE_USER_LINK = "div#site_content a[href='https://vacations-management.herokuapp.com/employees/new']";
+        this.CREATE_USER_LINK = "div#site_content a[href='/employees/new']";
+    }
+    async clickOnCreateEmployee() {
+        await this.driver.evaluate(scrollToSelector => {
+            document.querySelector(scrollToSelector).scrollIntoView();
+          }, this.CREATE_USER_LINK);
+        return this.driver.click(this.CREATE_USER_LINK);
     }
 }
 
