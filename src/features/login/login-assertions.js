73ca57d9041f9423cbfbc3416490a44dc684acab
jagarzone6 <jagarzone@gmail.com>
2019-08-11 21:54:11 -0500

    added assertions to login

--- src/features/login/login-assertions.js
+++ src/features/login/login-assertions.js
@@ -1,20 +1,26 @@
+let chai = require("chai");
+let chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
 class LoginAssertions {
     constructor(loginPage) {
         this.loginPage=loginPage;
+        this.assert = chai.assert;
+
     }
     async verifyLoginIsVisible() {
         return await Promise.all([
-            this.loginPage.driver.waitForSelector(this.loginPage.EMAIL_INPUT,{visible:true}),
-            this.loginPage.driver.waitForSelector(this.loginPage.PASS_INPUT,{visible:true}),
-            this.loginPage.driver.waitForSelector(this.loginPage.SUBMIT_BUTTON,{visible:true})
+            this.assert.isFulfilled(this.loginPage.driver.waitForSelector(this.loginPage.EMAIL_INPUT,{visible:true})),
+            this.assert.isFulfilled(this.loginPage.driver.waitForSelector(this.loginPage.PASS_INPUT,{visible:true})),
+            this.assert.isFulfilled(this.loginPage.driver.waitForSelector(this.loginPage.SUBMIT_BUTTON,{visible:true}))
         ]);
     }
 
     async validateHomePageAfterLogin(homePage) {
         return await Promise.all([
-            homePage.driver.waitForSelector(homePage.LOGGED_BANNER,{visible:true}),
-            homePage.driver.waitForSelector(homePage.LOGO,{visible:true}),
-            homePage.driver.waitForSelector(homePage.USER_LOGGED_SPAN,{visible:true})
+            this.assert.isFulfilled(homePage.driver.waitForSelector(homePage.LOGGED_BANNER,{visible:true})),
+            this.assert.isFulfilled(homePage.driver.waitForSelector(homePage.LOGO,{visible:true})),
+            this.assert.isFulfilled(homePage.driver.waitForSelector(homePage.USER_LOGGED_SPAN,{visible:true}))
         ]);
     }
 }
